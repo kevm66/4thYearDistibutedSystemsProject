86d47849f87f123443a2790ab5fbd6a62ea5f561
karry95 <karrz95@gmail.com>
2018-04-15 11:55:02 +0100

    Updated oven

--- Smart HomeSystem/src/main/java/clientui/UIConstants.java
+++
@@ -1,9 +0,0 @@
-package clientui;
-
-public class UIConstants {
-
-    public static final int UIWIDTH = 550;
-    public static final int UIHEIGHT = 600;
-    public static final int COMPONENTWIDTH = 520; //520 is default //450 makes buttons fit
-    public static final int CONTROLY = 425;
-}
